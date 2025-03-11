a45d9806d6fe1482d9cc65dbc4df082b8a775458
AurelGolemi <golemiaurel68@gmail.com>
2025-03-11 18:51:11 +0200

    -m "This is my calculator project" -m commit "Just a simple calculator"

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.querySelector('.display');
+let btn = document.querySelectorAll('.btn');
+let operator = document.querySelector('.btn-operator');
+// const btnClear = document.querySelector('.btn-clear');
+// const btnDelete = document.querySelector('.btn-delete');
+// const percent = document.querySelector('.btn-percent');
+// const divide = document.querySelector('.btn-divide');
+// const multiply = document.querySelector('.btn-multiply');
+// const subtract = document.querySelector('.btn-subtract');
+// const add = document.querySelector('.btn-add');
+// const decimal = document.querySelector('.btn-decimal');
+// const equal = document.querySelector('.btn-equal');
+
+// document.addEventListener('click', function(e) {
+//   if(e.target.classList.contains('btn')) {
+//     let value = e.target.getAttribute('data-num');
+//     display.value += value;
+//   }
+//   if(e.target.classList.contains('btn-clear')) {
+//     display.value = '';
+//   }
+//   if(e.target.classList.contains('btn-delete')) {
+//     display.value = display.value.slice(0, -1);
+//   }
+//   if(e.target.classList.contains('btn-percent')) {
+//     display.value = display.value / 100;
+//   }
+// })
